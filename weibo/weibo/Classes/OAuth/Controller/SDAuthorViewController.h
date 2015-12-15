7fd1c2418c5754524542f94bf87950f721703b2a
SudongJi <sudong@zaizhibo.tv>
2015-12-16 00:49:33 +0800

    增加授权控制器

---
+++ weibo/weibo/Classes/OAuth/Controller/SDAuthorViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDAuthorViewController.h
+//  weibo
+//
+//  Created by jisudong on 15/12/16.
+//  Copyright © 2015年 jisudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDAuthorViewController : UIViewController
+
+@end
---
+++ weibo/weibo/Classes/OAuth/Controller/SDAuthorViewController.m
@@ -0,0 +1,20 @@
+//
+//  SDAuthorViewController.m
+//  weibo
+//
+//  Created by jisudong on 15/12/16.
+//  Copyright © 2015年 jisudong. All rights reserved.
+//
+
+#import "SDAuthorViewController.h"
+
+@implementation SDAuthorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIWebView *webView = [[UIWebView alloc] init];
+}
+
+@end
